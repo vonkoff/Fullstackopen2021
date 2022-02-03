2179c57e6057b78e03d35a538c38e58e90d22d7b
Ivan Vukusic <jaaajaa102@gmail.com>
2022-02-03 13:31:59 -0500

    Add files via upload

---
+++ part4/controllers/blogs.js
@@ -0,0 +1,32 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (req, res) => {
+    Blog.find({}).then(blogs => {
+        res.json(blogs)
+    })
+})
+
+blogsRouter.get('/:id', (req, res, next) => {
+    Blog.findById(req.params.id)
+        .then(blog => {
+            if (blog) {
+                res.json(blog)
+            } else {
+                res.status(404).end()
+            }
+        })
+        .catch(err => next(err))
+})
+
+blogsRouter.post('/', (req,res) =>{
+    const blog = new Blog(req.body)
+
+    blog
+        .save()
+        .then(result => {
+            res.status(201).json(result)
+        })
+})
+
+module.exports = blogsRouter
