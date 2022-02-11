dd9eebf64acab55abbe92c6ff2d0d5e1135d07fa
Ivan Vukusic <jaaajaa102@gmail.com>
2022-02-10 23:41:59 -0500

    Update app.js

--- part4/app.js
+++ part4/app.js
@@ -7,6 +7,8 @@ const notesRouter = require('./controllers/blogs')
 // const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 // logger.info('connecting to', config.MONGODB_URI)
 
@@ -24,6 +26,8 @@ app.use(express.json())
 // app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 // app.use(middleware.unknownEndpoint)
 // app.use(middleware.errorHandler)
